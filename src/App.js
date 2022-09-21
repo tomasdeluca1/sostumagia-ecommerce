af4788e0ad0cc5d8af0aa987650f2095258c7aa9
tomasdelucaa <tomasdelucaa@gmail.com>
2022-09-20 23:13:12 -0300

    Archivo app.js con estilos y funciones

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
+import React, { useState } from "react";
 import "./App.css";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+// import Button from "./components/Button/Button";
+import ItemCount from "./components/ItemCount/ItemCount";
 
 function App() {
+  const [contador, setContador] = useState(0);
+  const stock = 12;
+  // let counter = contador;
+
+  const onDiminish = () => {
+    if (contador > 0) {
+      setContador(contador - 1);
+    }
+  };
+
+  const onAdd = () => {
+    if (contador < stock - 1) {
+      setContador(contador + 1);
+    } else if (contador == stock - 1) {
+      setContador(contador + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <NavBar />
-      <header className="App-header">
-        <img src="../../sos-tu-magia.png" className="App-logo" alt="logo" />
-        <p>
-          Sitio en proceso llevado a cabo por <strong>Huevsite</strong>.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/huevsite/?hl=es-la"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ir al perfil
-        </a>
-      </header>
+      <NavBar counter={contador} />
+      <ItemListContainer greeting={"Bienvenido a Sos tu magia"} stock={stock} />
+      <ItemCount onAdd={onAdd} onDiminish={onDiminish} />
     </div>
   );
 }
