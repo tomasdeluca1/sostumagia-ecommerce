9e4523003ac32140e3728e9c8eb0d8b3845ea257
tomasdelucaa <tomasdelucaa@gmail.com>
2022-10-04 21:33:15 -0300

    Products and functions from mock actualizated

---
+++ src/asyncMock.js
@@ -0,0 +1,91 @@
+const productos = [
+  {
+    id: "4",
+    name: "Volver a tu magia",
+    description:
+      "Dirigido a docentes y madres que quieran volver a mirarse, y regalarse un espacio de conexión emocional.",
+    price: "2.000 ARS",
+    category: "3",
+    type: "Grabado",
+    img: "../img/tu-magia.webp",
+    stock: 10,
+  },
+  {
+    id: "5",
+    name: "Crecimiento en el aula",
+    description: `Exploraremos presencialmente cómo acompañar el desarrollo de una "mente de chicle" en nuestros alumnos.`,
+    price: "5.500 ARS",
+    category: "1",
+    type: "Presencial",
+    img: "../img/educacion.jpeg",
+    stock: 25,
+  },
+  {
+    id: "6",
+    name: "Acuarela para jugar",
+    description:
+      "Disparadores para darle rienda suelta a tu creatividad, soltarte e invitarte a jugar. Y sí!!! Está dirigido a grandes!",
+    price: "3.600 ARS",
+    category: "1",
+    type: "Presencial",
+    img: "../img/acuarelas.jpeg",
+    stock: 100,
+  },
+  {
+    id: "1",
+    name: "Creatividad e innovación",
+    description:
+      "Herramientas para poder salir de bloqueos y salir adelante con nuevas e ingeniosas ideas.",
+    price: "5.500 ARS",
+    category: "2",
+    type: "Online",
+    img: "../img/creatividadInnovacion.jpg",
+    stock: 15,
+  },
+  {
+    id: "2",
+    name: "Educación emocional",
+    description:
+      "Herramientas para crear espacios de conexión emocional en el aula y en el consultorio.",
+    price: "4.200 ARS",
+    category: "2",
+    type: "Online",
+    img: "../img/educacionEmocional.jpg",
+    stock: 25,
+  },
+  {
+    id: "3",
+    name: "Autoconocimiento",
+    description:
+      "Guía ayudarte a encontrar a tu yo interior, un curso para realizar con vos mismo.",
+    price: "3.600 ARS",
+    category: "3",
+    type: "Grabado",
+    img: "../img/autoconocimiento.jpg",
+    stock: 100,
+  },
+];
+
+export const getProducts = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(productos);
+    }, 2000);
+  });
+};
+
+export const getProduct = (itemId) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(productos.find((prod) => prod.id === itemId));
+    }, 2000);
+  });
+};
+
+export const getProductsByCategory = (categoryId) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(productos.filter((prod) => prod.category === categoryId));
+    }, 2000);
+  });
+};
