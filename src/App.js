e97651668f6ef5635969d657fb483832fcd60eb0
tomasdelucaa <tomasdelucaa@gmail.com>
2022-10-04 21:30:18 -0300

    Routes configured

--- src/App.js
+++ src/App.js
@@ -2,34 +2,64 @@ import React, { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 // import Button from "./components/Button/Button";
 import ItemCount from "./components/ItemCount/ItemCount";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [contador, setContador] = useState(0);
-  const stock = 12;
-  // let counter = contador;
+  // const [contador, setContador] = useState(0);
+  // const stock = 12;
+  // // let counter = contador;
 
-  const onDiminish = () => {
-    if (contador > 0) {
-      setContador(contador - 1);
-    }
-  };
+  // const onDiminish = () => {
+  //   if (contador > 0) {
+  //     setContador(contador - 1);
+  //   }
+  // };
 
-  const onAdd = () => {
-    if (contador < stock - 1) {
-      setContador(contador + 1);
-    } else if (contador == stock - 1) {
-      setContador(contador + 1);
-    }
-  };
+  // const onAdd = () => {
+  //   if (contador < stock - 1) {
+  //     setContador(contador + 1);
+  //   } else if (contador == stock - 1) {
+  //     setContador(contador + 1);
+  //   }
+  // };
 
   return (
-    <div className="App">
-      <NavBar counter={contador} />
-      <ItemListContainer greeting={"Bienvenido a Sos tu magia"} stock={stock} />
-      <ItemCount onAdd={onAdd} onDiminish={onDiminish} />
-    </div>
+    // <Route
+    //   path="/"
+    //   element={
+    //     <>
+    //       {" "}
+    //       <ItemListContainer greeting={"bienvenido"} />
+    //       <ItemDetailContainer greeting={"Mercado Libre"} />
+    //       {" "}
+    //     </>
+    //   }
+    // />
+    <BrowserRouter>
+      <div className="App">
+        <NavBar /*counter={contador}*/ />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ItemListContainer greeting={"¡Bienvenido a sos tu magia!"} />
+            }
+          />
+          <Route
+            path="/category/:categoryId"
+            element={
+              <ItemListContainer greeting={"Bienvenido a la categoría:"} />
+            }
+          />
+          <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+          {/* <Route path="/cart" element={<Cart />} /> */}
+          {/* <Route path="/checkout" element={<Checkout />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
