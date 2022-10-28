99d46b988fe055e86768e795dc7cd139490de870
tomasdelucaa <tomasdelucaa@gmail.com>
2022-10-28 16:07:45 -0300

    Checkout logic finished, with validations

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,78 +1,22 @@
-import { useState, useContext } from "react";
-import { CartContext } from "../../context/cartContext/CartContext";
-import { NotificationContext } from "../../context/NotificationContext/Notification";
-import {
-  addDoc,
-  collection,
-  doc,
-  updateDoc,
-  getDocs,
-  where,
-  query,
-  documentId,
-  writeBatch,
-} from "firebase/firestore";
-import { db } from "../../services/firebase/index";
+import { useState } from "react";
+import { useCart } from "../../context/cartContext/CartContext";
+import { useNotification } from "../../context/NotificationContext/Notification";
 import "./checkout.css";
 import CheckoutForm from "./Form/CheckoutForm";
+import { createOrder } from "../../services/firebase/firestore";
 
 const Checkout = () => {
   const [loading, setLoading] = useState(false);
 
-  const { cart, getTotal } = useContext(CartContext);
-  const { setNotification } = useContext(NotificationContext);
+  const { cart, getTotal } = useCart();
+  const { setNotification } = useNotification();
 
   const totalPrice = getTotal();
 
-  const createOrder = async (clientData) => {
+  const generateOrder = (clientData) => {
     setLoading(true);
-
     try {
-      const objOrder = {
-        buyer: clientData,
-        items: cart,
-        date: new Date(),
-        totalPrice,
-      };
-
-      console.log(objOrder);
-
-      const ids = cart.map((prod) => prod.id);
-      const productsRef = collection(db, "products");
-
-      const productsAddedFromFirestore = await getDocs(
-        query(productsRef, where(documentId(), "in", ids))
-      );
-      const { docs } = productsAddedFromFirestore;
-
-      const batch = writeBatch(db);
-      const outOfStock = [];
-
-      docs.forEach((doc) => {
-        const dataDoc = doc.data();
-        const stockDb = dataDoc.stock;
-
-        const productAddedToCart = cart.find((prod) => prod.id === doc.id);
-        const prodQuantity = productAddedToCart?.quantity;
-
-        if (stockDb >= prodQuantity) {
-          batch.update(doc.ref, { stock: stockDb - prodQuantity });
-        } else {
-          outOfStock.push({ id: doc.id, ...dataDoc });
-        }
-      });
-
-      if (outOfStock.length === 0) {
-        await batch.commit();
-
-        const orderRef = collection(db, "orders");
-        const orderAdded = await addDoc(orderRef, objOrder);
-
-        setNotification("success", `El id de su orden es: ${orderAdded.id}`);
-        // console.log(`El id de su orden es: ${orderAdded.id}`)
-      } else {
-        setNotification("fail", "Hay productos fuera de stock");
-      }
+      createOrder(clientData, cart, totalPrice, setNotification);
     } catch (error) {
       console.log(error);
     } finally {
@@ -99,7 +43,7 @@ const Checkout = () => {
     return (
       <div>
         <h2 className="checkoutTitle">Datos del comprador:</h2>
-        <CheckoutForm createOrder={createOrder} cart={cart} />
+        <CheckoutForm generateOrder={generateOrder} />
       </div>
     );
   }
--- src/components/Checkout/Form/CheckoutForm.js
+++ src/components/Checkout/Form/CheckoutForm.js
@@ -1,53 +1,104 @@
-import "./form.css"
-import {useContext, useState} from "react"
-import {useForm} from "react-hook-form"
-import {NotificationContext} from "../../../context/NotificationContext/Notification"
+import "./form.css";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-const CheckoutForm = ({createOrder, cart}) =>{
+const CheckoutForm = ({ generateOrder }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm();
 
-    const {register, handleSubmit, formState:{errors}} = useForm()
+  const [emailConfirmated, setEmailConfirmated] = useState(false);
 
-    const {setNotification} = useContext(NotificationContext)
+  const onSubmit = (data, e) => {
+    Object.keys(errors).length === 0 && emailConfirmated
+      ? generateOrder(data)
+      : e.preventDefault();
+  };
 
-    const onSubmit = data =>{
-        createOrder(data)
-    }
+  const emailConfirmation = () => {
+    const email = getValues("email");
+    const email2 = getValues("email2");
 
-    return(
-        <div>
-            <form className="checkoutForm" onSubmit= {handleSubmit(onSubmit)}>
-               
-            
-                <input
-                type="text"
-                {...register("nombre", {required:true, maxLength:20})}
-             
-                placeholder="Nombre completo"
-                className="checkoutInput"
-                />
-                   {errors.nombre?.type === "required" && <p className="errorsForm">Nombre completo requerido</p>}
-                <input
-                type="number"
-                {...register("telegono", {required:true, minLength: 10,maxLength:13})}
-                placeholder="1125085500"
-                className="checkoutInput"
-                />
-                   {errors.telegono?.type === "required" && <p className="errorsForm">El número de teléfono está vacío</p>}
-               {errors.telegono?.type === "minLength" && <p className="errorsForm">Debe tener un mínimo de 10 dígitos</p>}
-               {errors.telegono?.type === "maxLength" && <p className="errorsForm">Debe tener un máximo de 13 dígitos</p>}
-                <input
-                type="text"
-                {...register("email", {required:true, pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i})}
-                placeholder="Email"
-                className="checkoutInput"
-                />
-                {errors.email?.type === "required" && <p className="errorsForm">El email es requerido</p>}
-               {errors.email?.type === "pattern" && <p className="errorsForm">El formato del mail es inválido</p>}
+    email === email2 ? setEmailConfirmated(true) : setEmailConfirmated(false);
+  };
 
-                 <input type="submit" className="checkoutSubmit"  />
-         </form>
-        </div>
-    )
-}
+  return (
+    <div>
+      <form className="checkoutForm" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          {...register("nombre", { required: true, maxLength: 20 })}
+          placeholder="Nombre completo"
+          className="checkoutInput"
+          onBlur={emailConfirmation}
+        />
+        {errors.nombre?.type === "required" && (
+          <p className="errorsForm">Nombre completo requerido</p>
+        )}
+        <input
+          type="number"
+          {...register("telefono", {
+            required: true,
+            minLength: 10,
+            maxLength: 13,
+          })}
+          placeholder="1125085500"
+          className="checkoutInput"
+          onBlur={emailConfirmation}
+        />
+        {errors.telefono?.type === "required" && (
+          <p className="errorsForm">El número de teléfono está vacío</p>
+        )}
+        {errors.telefono?.type === "minLength" && (
+          <p className="errorsForm">Debe tener un mínimo de 10 dígitos</p>
+        )}
+        {errors.telefono?.type === "maxLength" && (
+          <p className="errorsForm">Debe tener un máximo de 13 dígitos</p>
+        )}
+        <input
+          type="text"
+          {...register("email", {
+            required: true,
+            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+          })}
+          placeholder="Email"
+          className="checkoutInput"
+          onBlur={emailConfirmation}
+        />
+        {errors.email?.type === "required" && (
+          <p className="errorsForm">El email es requerido</p>
+        )}
+        {errors.email?.type === "pattern" && (
+          <p className="errorsForm">El formato del mail es inválido</p>
+        )}
+        <input
+          type="text"
+          {...register("email2", {
+            required: true,
+            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+          })}
+          placeholder="Confirmar email"
+          className="checkoutInput"
+          onBlur={emailConfirmation}
+        />
+        {errors.email2?.type === "required" && (
+          <p className="errorsForm">La confirmación del mail es requerida</p>
+        )}
+        {errors.email2?.type === "pattern" && (
+          <p className="errorsForm">El formato del mail es inválido</p>
+        )}
+        {!emailConfirmated && (
+          <p className="errorsForm">
+            El mail de confirmación debe ser el mismo
+          </p>
+        )}
+        <input type="submit" className="checkoutSubmit" />
+      </form>
+    </div>
+  );
+};
 
-export default CheckoutForm
+export default CheckoutForm;
