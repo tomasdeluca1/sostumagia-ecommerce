165d243186689b6cced65c2098e6f9f7c15d1633
Tomás Deluca <Tomasdelucaa@gmail.com>
2024-12-20 17:43:34 -0300

    meta tags

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HelmetProvider } from "react-helmet-async";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
@@ -14,45 +15,50 @@ import TratamientoPsicopedagogico from "./components/pages/TratamientoPsicopedag
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col w-full bg-[#fed5d5]">
-      <NotificationContextProvider>
-        <CartContextProvider>
-          <BrowserRouter>
-            <NavBar />
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route
-                  path="/cursos"
-                  element={
-                    <ItemListContainer greeting="Bienvenido a la categoría:" />
-                  }
-                />
-                <Route path="/supervisiones" element={<Supervisiones />} />
-                <Route
-                  path="/tratamiento-psicopedagogico"
-                  element={<TratamientoPsicopedagogico />}
-                />
-                <Route path="/item/:itemId" element={<ItemDetailContainer />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route
-                  path="*"
-                  element={
-                    <div className="flex items-center justify-center h-screen">
-                      <h1 className="text-4xl font-bold text-gray-800">
-                        404 NOT FOUND
-                      </h1>
-                    </div>
-                  }
-                />
-              </Routes>
-            </main>
-            <Footer />
-          </BrowserRouter>
-        </CartContextProvider>
-      </NotificationContextProvider>
-    </div>
+    <HelmetProvider>
+      <div className="min-h-screen flex flex-col w-full bg-[#fed5d5]">
+        <NotificationContextProvider>
+          <CartContextProvider>
+            <BrowserRouter>
+              <NavBar />
+              <main className="flex-grow">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route
+                    path="/cursos"
+                    element={
+                      <ItemListContainer greeting="Bienvenido a la categoría:" />
+                    }
+                  />
+                  <Route path="/supervisiones" element={<Supervisiones />} />
+                  <Route
+                    path="/tratamiento-psicopedagogico"
+                    element={<TratamientoPsicopedagogico />}
+                  />
+                  <Route
+                    path="/item/:itemId"
+                    element={<ItemDetailContainer />}
+                  />
+                  <Route path="/cart" element={<Cart />} />
+                  <Route path="/checkout" element={<Checkout />} />
+                  <Route
+                    path="*"
+                    element={
+                      <div className="flex items-center justify-center h-screen">
+                        <h1 className="text-4xl font-bold text-gray-800">
+                          404 NOT FOUND
+                        </h1>
+                      </div>
+                    }
+                  />
+                </Routes>
+              </main>
+              <Footer />
+            </BrowserRouter>
+          </CartContextProvider>
+        </NotificationContextProvider>
+      </div>
+    </HelmetProvider>
   );
 }
 
